9c7ee138c2502527f58908ec93421d3230a8ae30
Bart Lantz <underhilllabs@gmail.com>
2019-10-14 13:37:20 -0600

    adds exerise scaffold

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  resources :exercises
   resources :workout_sessions
   devise_for :users
   get 'home/index'
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_10_14_190733) do
+ActiveRecord::Schema.define(version: 2019_10_14_193547) do
+
+  create_table "exercises", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
